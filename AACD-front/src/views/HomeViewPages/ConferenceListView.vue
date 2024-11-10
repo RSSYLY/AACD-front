ba4a262b2f902b1a448a874c165bb4f44f3d5887
BoyNextDoor♂ <59304676+RSSYLY@users.noreply.github.com>
2024-11-11 01:23:16 +0800

    md'

--- AACD-front/src/views/HomeViewPages/ConferenceListView.vue
+++ AACD-front/src/views/HomeViewPages/ConferenceListView.vue
@@ -111,8 +111,14 @@ export default {
         dialog.open = true;
         // 设置对话框内容，包含会议信息，会议信息会在对话框中显示，这里只是简单的设置了一个字符串，实际上应该是一个包含会议信息的对象， 例如 { title: '会议标题', content: '会议内容' }，这个对象应该是从后端获取的，  也可以是一个包含会议信息的对象数组，这样可以实现在对话框中显示多个会议信息，  例如 [{ title: '会议1标题', content: '会议1内容' }, { title: '会议2标题', content: '会议2内容' }]
         this.showingConference = conference;}
+    },
+    closeConferenceInfoDialog() {
+      // 关闭会议详情对话框
+      const dialog = document.querySelector('.con-info-dialog');
+      dialog.open = false;
     }
-    }
+    },
+
 
 }
 </script>
@@ -143,9 +149,9 @@ export default {
                   <div class="main-list-item-cover">
                     <div class="main-list-item-cover-img" v-for="img in conference.coverImg" :style="{backgroundImage:'url('+ this.baseURL +img.url+')'}"></div>
                   </div>
-                  <div class="main-list-item-host">{{ conference.host }}</div>
-                  <div class="main-list-item-title">{{ conference.f_7e25tpohq5o }}</div>
-                  <div class="main-list-item-description">{{ conference.f_n3z4nrdlxd2 }}</div>
+                  <div class="main-list-item-host">{{ conference.host || "无举办方信息" }}</div>
+                  <div class="main-list-item-title">{{ conference.f_7e25tpohq5o || "无标题" }}</div>
+                  <div class="main-list-item-description">{{ conference.f_n3z4nrdlxd2 || "无描述"}}</div>
                 </div>
               </div>
             </div>
@@ -154,8 +160,17 @@ export default {
       </div>
     </div>
   </div>
-  <mdui-dialog fullscreen class="con-info-dialog" close-on-esc="true">
-    <Md :md-content="this.showingConference.content" />
+  <mdui-dialog fullscreen class="con-info-dialog" close-on-esc="true" >
+    <div class="dialog-header">
+      <mdui-button-icon icon="arrow_back" @click="closeConferenceInfoDialog"></mdui-button-icon>
+      <div class="dialog-title">{{ showingConference.f_7e25tpohq5o }}</div>
+    </div>
+    <div class="dialog-content">
+      <div class="dialog-cover">
+        <div class="dialog-cover-img" v-for="img in showingConference.coverImg" :style="{backgroundImage:'url('+ this.baseURL +img.url+')'}"></div>
+      </div>
+      <Md :md-content="this.showingConference.content" />
+    </div>
   </mdui-dialog>
 
 </template>
@@ -248,7 +263,6 @@ export default {
   font-size: var(--mdui-typescale-label-medium-size);
   font-weight: var(--mdui-typescale-label-small-weight);
   background-color: rgb(var(--mdui-color-secondary-container));
-  //border: 1px solid rgb(var(--mdui-color-on-secondary-container));
   display: flex;
   align-items: center;
   color:rgb(var(--mdui-color-secondary))
@@ -264,14 +278,23 @@ export default {
   border-radius: var(--mdui-shape-corner-medium);
 }
 .main-list-item-host{
-  padding: 20px 10px 10px 10px;
+  border-radius: var(--mdui-shape-corner-extra-large);
+  padding: 10px 15px;
+  margin: 10px;
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  background-color: rgb(var(--mdui-color-surface-bright));
   font-size: var(--mdui-typescale-label-medium-size);
   font-weight: var(--mdui-typescale-label-small-weight);
+  bottom:10px;
+
+
 }
 .main-list-item-title{
   padding: 10px;
   font-size: var(--mdui-typescale-title-large-size);
-  font-weight: var(--mdui-typescale-title-large-weight);
+  font-weight: var(--mdui-typescale-title-medium-weight);
 }
 .main-list-item-description{
   padding: 10px;
@@ -284,6 +307,58 @@ export default {
   }
 }
 
+.con-info-dialog {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+mdui-dialog::part(panel){
+  padding: 0;
+}
+.dialog-header{
+  display: flex;
+  justify-content: flex-start;
+  gap:10px;
+  padding: 20px;
+  align-items: center;
+}
+.dialog-title{
+  font-size: var(--mdui-typescale-title-large-size);
+  font-weight: var(--mdui-typescale-title-medium-weight);
+  flex: 1;
+  align-items: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+}
+.dialog-cover{
+  display: flex;
+  justify-content: center;
+  margin-bottom: 40px;
+}
+.dialog-cover-img{
+  width: 100%;
+  height: 200px;
+  max-width: 400px;
+  background-size: cover;
+  background-position: center;
+  border-radius: var(--mdui-shape-corner-medium);
+}
+.dialog-content{
+  height: calc(100% - 160px);
+  overflow-y: auto;
+  padding: 40px;
+  border-radius: var(--mdui-shape-corner-extra-large);
+  background-color: rgb(var(--mdui-color-surface-bright));
+}
+
+@media screen and (max-width: 600px){
+  .dialog-content{
+    padding: 20px;
+    height: calc(100% - 120px);
+  }
+}
 
 
 </style>
